b5cd898cf0c4ad4fae809bd495310c797bcdd624
Matias <64340009+CineraM@users.noreply.github.com>
2023-02-13 00:09:25 -0500

    final

--- lab2/Lab2_epuck/controllers/Lab2_Task1/Lab2_Task1.py
+++ lab2/Lab2_epuck/controllers/Lab2_Task1/Lab2_Task1.py
@@ -1,3 +1,11 @@
+"""
+Matias Cinera - U 6931_8506
+CAP-6626
+Instructor: Dr.Alfredo Weitzenfeld 
+Ta:         Chance Hamilton
+Assigment:  lab2_task1 controller.
+"""
+
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
@@ -8,12 +16,10 @@ import math
 # Creates Robot
 #######################################################
 robot = Robot()
-
 #######################################################
 # Sets the time step of the current world
 #######################################################
 timestep = int(robot.getBasicTimeStep())
-
 #######################################################
 # Gets Robots Distance Sensors
 #######################################################
@@ -47,78 +53,133 @@ leftposition_sensor = robot.getDevice('left wheel sensor')
 rightposition_sensor = robot.getDevice('right wheel sensor')
 leftposition_sensor.enable(timestep)
 rightposition_sensor.enable(timestep)
-
 imu = robot.getDevice('inertial unit')
 imu.enable(timestep)
+#######################################################
 
+# global variables
 distBtwWhe = 2.28
-distBtwWheR = distBtwWhe/2
+dmid = distBtwWhe/2
 w_dia = 1.6
 w_r = w_dia/2
 pi = math.pi
+indx = 0
+poseTimes = []
+target_time = 999
+
+################## - GETTERS & HELPER - ######################
+def getPositionSensors():
+    return leftposition_sensor.getValue(), rightposition_sensor.getValue()
+
+def imu_cleaner(imu_reading):
+    rad_out = imu_reading
+    if rad_out < 0:
+        rad_out = rad_out + 2*math.pi
+    return math.degrees(rad_out)
+
+def imu_rad():
+    return math.radians(imu_cleaner(imu.getRollPitchYaw()[2]))
 
 # Transform inches to rad - max wheel speed is 5.024 in/
 def inToRad(v):
-    v = v/w_r                # input=inches
-    if v > 6.28:             # max rotational velocity
-        print(f'ERROR: Invalid input ({v:.2f} > 6.28), setting it to 6.28')
-        v = 6.28
-    elif v < -6.28:
-        print(f'ERROR: Invalid input ({v:.2f} < -6.28), setting it to -6.28')
-        v = -6.28
-    return v
+    return v/w_r
 
-# move either left, or right motor with speed v
-def singleMotorMotionV(v, isLeft):
-    if(isLeft):
-        leftMotor.setVelocity(v)
+################## - POSITION FUNCTIONS - ######################
+# Return the distance in in/s based on the R and L Wheel speed
+def distAfterTask(vl, vr):
+    vl = round(vl, 8)
+    vr = round(vr, 8)
+    
+    if vl == vr:  
+        return 0.032*vl, 0.032*vr
+    if vl == -vr or math.isnan(vl):
+        return 0, 0
     else:
-        rightMotor.setVelocity(v)
+        # circular motions turns
+        # vl = ( (cur_l-prev_l) /0.032) * wheel_radius
+        v = (vr+vl)/2
+        S = v*0.032
+        x = S*math.cos(imu.getRollPitchYaw()[2])
+        y = S*math.sin(imu.getRollPitchYaw()[2])
+        return abs(x), abs(y)
 
+# Position variables
+cur_position = [-5,-15, 0] 
+prev_l, prev_r = getPositionSensors()
+signs = [0, 0]
 
+def robotPose():
+    print(f'Time: {robot.getTime():.2f}s - Robot Position: [{cur_position[0]:.2f}, {cur_position[1]:.2f}, {cur_position[2]:.2f}]')
+    print("------------------------------------------------------")
+
+def updateRobotPose():
+    global prev_l, prev_r, target_time, indx
+    cur_l, cur_r = getPositionSensors()
+    # 0.032 = time step
+    vl = (cur_l-prev_l)/0.032
+    vr = (cur_r-prev_r)/0.032
+    
+    x, y = distAfterTask(vl*w_r, vr*w_r)
+
+    if signs[0] == 1:
+        cur_position[0] += x
+    elif signs[0] == -1:
+        cur_position[0] -= x
+    
+    # print(f'x: {x}, y: {y}')
+    if signs[1] == 1:
+        cur_position[1] += y
+    elif signs[1] == -1:
+        cur_position[1] -= y
+    prev_l = cur_l
+    prev_r = cur_r
+    
+    cur_position[2] = imu_cleaner(imu.getRollPitchYaw()[2])
+
+    if robot.getTime() >= target_time:
+        robotPose()
+        indx+=1
+        try:
+            target_time = poseTimes[indx]
+        except:
+            target_time = 9999
+
+##################################################################    
+    
+################## - MOTION FUNCTIONS - ######################
 # move in straing line at max speed = 5.024 in/s
 def straightMotionD(d):
-    v = 6.28
-    time = d/5.024
-
+    v = 6.28        # use max speed
+    time = d/5.024  # 5.024 = v*r ==> max linear speed
     s_time = robot.getTime()
     while robot.step(timestep) != -1:
         if robot.getTime()-s_time > time:
             leftMotor.setVelocity(0)
             rightMotor.setVelocity(0)
+            updateRobotPose()
             break 
         leftMotor.setVelocity(v)
         rightMotor.setVelocity(v)
-
-# move the left and right motors by a specified speed and period of time 
-# def dualMotorMotionLRT(l, r, t):
-#     l = inToRad(l, True)        
-#     r = inToRad(r, False) 
-
-#     print(f'Task [vl: {l:.2f}, vr: {r:.2f}, t: {t:.2f}] - Start-time: {robot.getTime()}')
-#     singleMotorMotionV(l, True)
-#     singleMotorMotionV(r, False)
-#     robot.step(int(t*1000))
-#     singleMotorMotionV(0, True)
-#     singleMotorMotionV(0, False)
-#     # return l, r
-
+        updateRobotPose()
+        
+ 
 def rotationInPlace(direction):
     # quarter of a circle = 2pi/4  == pi/2
     # for this self rotation ICC R = d_mid
-    s = pi*distBtwWheR / 2
+    s = pi*dmid / 2
     # s = v * t
     in_v = 0.4
     time = s/in_v
     # time = 15
     # in_v = s/time
-    v = inToRad(in_v)
+    v = inToRad(in_v) # input must be less than 6.28
 
     s_time = robot.getTime()
     while robot.step(timestep) != -1:
         if robot.getTime()-s_time > time:
             leftMotor.setVelocity(0)
             rightMotor.setVelocity(0)
+            updateRobotPose()
             break 
         if direction == "left":
             leftMotor.setVelocity(-v)
@@ -126,18 +187,17 @@ def rotationInPlace(direction):
         else:
             leftMotor.setVelocity(v)
             rightMotor.setVelocity(-v)
+        updateRobotPose()
 
 def circularMotion(direction):
     vr = 2
     R = 5   # radius of the circle, hardcoded for this task.
-    
-    omega = vr/(R+distBtwWheR)
-    vl = omega*(R-distBtwWheR)
+    omega = vr/(R+dmid)
+    vl = omega*(R-dmid)
     v = (vr+vl)/2
-    # 2.5 R of icc
     s = (pi/2) * R
     time = s/v
-
+ 
     vl_rad = inToRad(vl)
     vr_rad = inToRad(vr) 
 
@@ -150,39 +210,17 @@ def circularMotion(direction):
         if robot.getTime()-s_time > time:
             leftMotor.setVelocity(0)
             rightMotor.setVelocity(0)
+            updateRobotPose()
             break 
         leftMotor.setVelocity(vl_rad)
         rightMotor.setVelocity(vr_rad)
-
-def diffDrive(arr):
-    pass
-
-
-def robotPose(A):
-    pass
-
-# Cleans the IMU readings so that the are in degrees and in the
-# range of [0,359]
-def imu_cleaner(imu_reading):
-    rad_out = imu_reading
-    if rad_out < 0:
-        rad_out = rad_out + 2*math.pi
-    return math.degrees(rad_out)
-
-
-def get_info():
-    # Process sensor data here.   
-    print("Left position sensor: " +str(leftposition_sensor.getValue()))
-    print("Right position sensor: " +str(rightposition_sensor.getValue()))
-    print("IMU: "+str(imu_cleaner(imu.getRollPitchYaw()[2])))
-    print("--------------------------------------------------")
-
+        updateRobotPose()
+##################################################################
 
 def getRobotCommands(A):
     cur_pose = "east"   # default starting position for this lab
-    commands = []       # should start and change based on the IMU
+    commands = []
     prev = []
-
     for i in range(len(A)):
         if i == 0:
             prev = A[i]
@@ -192,112 +230,83 @@ def getRobotCommands(A):
             # left to right
             if prev[0] < A[i][0]: # looking east 2
                 if cur_pose == "north":
-                    commands.append(0)
+                    commands.append([0, 0, 0])
                 elif cur_pose == "south":
-                    commands.append(-1) 
-                cur_pose = "east"          
-            
+                    commands.append([-1, 0, 0]) 
+                cur_pose = "east"
+                commands.append([abs(prev[0] - A[i][0]), 1, 0])           
             # right to left
-            else: # looking west = 4, 
+            else: # looking west
                 if cur_pose == "north":
-                    commands.append(-1)
+                    commands.append([-1, 0, 0])
                 elif cur_pose == "south":
-                    commands.append(0) 
-                cur_pose = "west"          
-            
-            commands.append(abs(prev[0] - A[i][0]))               
+                    commands.append([0, 0, 0]) 
+                cur_pose = "west"
+                commands.append([abs(prev[0] - A[i][0]), -1, 0])            
         # vertical movement
         elif prev[0] == A[i][0] and prev[1] != A[i][1]:
             # going up
             if prev[1] < A[i][1]:   
                 if cur_pose == "east":
-                    commands.append(-1)
+                    commands.append([-1, 0, 0]) 
                 elif cur_pose == "west":
-                    commands.append(0) 
-                cur_pose = "north"          
+                    commands.append([0, 0, 0]) 
+                cur_pose = "north"
+                commands.append([abs(prev[1] - A[i][1]), 0, 1])          
             # going down
             else:                   
                 if cur_pose == "east":
-                    commands.append(0)
+                    commands.append([0, 0, 0])
                 elif cur_pose == "west":
-                    commands.append(-1) 
+                    commands.append([-1, 0, 0])
                 cur_pose = "south"          
-            
-            commands.append(abs(prev[1] - A[i][1])) 
-        # hardcoded moves that are not straight lines    
-        # 1/4 circle moves
+                commands.append([abs(prev[1] - A[i][1]), 0, -1]) 
+        # 1/4 circle moves - hardcoded moves that are not straight lines 
         elif prev[0] != A[i][0] and prev[1] != A[i][1]:
             if cur_pose == "west":
-                commands.append(-3)
+                commands.append([-3, -1, -1])
                 cur_pose = "south"
             elif cur_pose == "east":
-                commands.append(-2)
+                commands.append([-2, 1, -1])
                 cur_pose = "south"
             elif cur_pose == "south":
-                commands.append(-2)
+                commands.append([-2, -1, -1])
                 cur_pose = "west"
         prev = A[i]
 
     return commands
-
-# no 180 deg turns in this project
-def robotWayPointNav(A):
-    # commands = [20, -1, 30, -1, 25, -3, 5, -1, 15, -2, -2, 10, -3, 5]
-    # ^ it should return those commands 
+    
+# commands = [20, -1, 30, -1, 25, -3, 5, -1, 15, -2, -2, 10, -3, 5] --> commands should be this
+# commands = [command, x-change, y-change], x and y changes can either be -1, 0 or 1.
+def robotWayPointNav(A, T=[]):
+    global poseTimes, target_time, indx
+    poseTimes = T
+    target_time = T[indx]
     commands = getRobotCommands(A)
-    # -1 rotate in place left, 0 for right
-    # -3 quarter of a circle left, -2 right
-    # positive values X --> straight motion for X distance
     for it in commands:
-        if it == -1:
+        global signs
+        signs = [it[1], it[2]]
+        ctype = it[0]
+        if ctype == -1:   # rotate in place left
             rotationInPlace("left")
-        elif it == 0:
+        elif ctype == 0:  # rotate in place right
             rotationInPlace("right")
-        elif it == -3:
+        elif ctype == -3: # quarter of a circle left
             circularMotion("left")
-        elif it == -2:
+        elif ctype == -2: # quarter of a circle right
             circularMotion("right")
-        else:
-            straightMotionD(it)
-        get_info()
+        else:       # else its a straight motion
+            straightMotionD(ctype)
+
 
 # get the initial position of the robot 
 def main():
     A = [[-5,-15],[15,-15],[15,15], [-10,15] ,[-15,10] ,[-15,5], [0,5] ,[5, 0] , [0,-5], [-10,-5] ,[-15,-10] ,[-15,-15]]
-    robotWayPointNav(A)
-
+    robotWayPointNav(A, [1, 2, 3, 5, 10, 15, 20, 23, 30])
     # spin :)
     while robot.step(timestep) != -1:
         leftMotor.setVelocity(3)
         rightMotor.setVelocity(-3)
         
-
-
-
 if __name__ == "__main__":
     main()
-
-#IGNORE THIS CODE ####################################################3
-# Main loop:
-# perform simulation steps until Webots is stopping the controller
-
-# while robot.step(timestep) != -1:
-#     # Read the sensors:
-#     # Enter here functions to read sensor data, like:
-#     # val = ds.getValue()
-
-#     # print simulation time in sec (timesteps are in msec)
-#     print("Time: " +str(robot.getTime()))
-    
-#     # Process sensor data here.   
-#     print("Left position sensor: " +str(leftposition_sensor.getValue()))
-#     print("Right position sensor: " +str(rightposition_sensor.getValue()))
-
-#     # Enter here functions to send actuator commands, like:
-#     #  motor.setPosition(10.0)
-#     leftMotor.setVelocity(6.28)
-#     rightMotor.setVelocity(6.28)
-    
-#     print("IMU: "+str(imu.getRollPitchYaw()[2]))
-    
-# # Enter here exit cleanup code.
--- lab2/Lab2_epuck/controllers/Lab2_Task1/test.py
+++ lab2/Lab2_epuck/controllers/Lab2_Task1/test.py
@@ -27,70 +27,88 @@
 # n, e, s, w
 
 # no 180 deg turns in this project
-def getRobotCommands(A):
-    cur_pose = "east"
-    commands = []
-    prev = []
+# def getRobotCommands(A):
+#     cur_pose = "east"
+#     commands = []
+#     prev = []
 
-    for i in range(len(A)):
-        if i == 0:
-            prev = A[i]
-            continue
-        # horizontal movement
-        if prev[0] != A[i][0] and prev[1] == A[i][1]:
-            # left to right
-            if prev[0] < A[i][0]: # looking east 2
-                if cur_pose == "north":
-                    commands.append(0)
-                elif cur_pose == "south":
-                    commands.append(-1) 
-                cur_pose = "east"          
+#     for i in range(len(A)):
+#         if i == 0:
+#             prev = A[i]
+#             continue
+#         # horizontal movement
+#         if prev[0] != A[i][0] and prev[1] == A[i][1]:
+#             # left to right
+#             if prev[0] < A[i][0]: # looking east 2
+#                 if cur_pose == "north":
+#                     commands.append(0)
+#                 elif cur_pose == "south":
+#                     commands.append(-1) 
+#                 cur_pose = "east"          
             
-            # right to left
-            else: # looking west = 4, 
-                if cur_pose == "north":
-                    commands.append(-1)
-                elif cur_pose == "south":
-                    commands.append(0) 
-                cur_pose = "west"          
+#             # right to left
+#             else: # looking west = 4, 
+#                 if cur_pose == "north":
+#                     commands.append(-1)
+#                 elif cur_pose == "south":
+#                     commands.append(0) 
+#                 cur_pose = "west"          
             
-            commands.append(abs(prev[0] - A[i][0]))               
-        # vertical movement
-        elif prev[0] == A[i][0] and prev[1] != A[i][1]:
-            # going up
-            if prev[1] < A[i][1]:   
-                if cur_pose == "east":
-                    commands.append(-1)
-                elif cur_pose == "west":
-                    commands.append(0) 
-                cur_pose = "north"          
-            # going down
-            else:                   
-                if cur_pose == "east":
-                    commands.append(0)
-                elif cur_pose == "west":
-                    commands.append(-1) 
-                cur_pose = "south"          
+#             commands.append(abs(prev[0] - A[i][0]))               
+#         # vertical movement
+#         elif prev[0] == A[i][0] and prev[1] != A[i][1]:
+#             # going up
+#             if prev[1] < A[i][1]:   
+#                 if cur_pose == "east":
+#                     commands.append(-1)
+#                 elif cur_pose == "west":
+#                     commands.append(0) 
+#                 cur_pose = "north"          
+#             # going down
+#             else:                   
+#                 if cur_pose == "east":
+#                     commands.append(0)
+#                 elif cur_pose == "west":
+#                     commands.append(-1) 
+#                 cur_pose = "south"          
             
-            commands.append(abs(prev[1] - A[i][1])) 
-        # 1/4 circle moves
-        elif prev[0] != A[i][0] and prev[1] != A[i][1]:
-            if cur_pose == "west":
-                commands.append(-3)
-                cur_pose = "south"
-            elif cur_pose == "east":
-                commands.append(-2)
-                cur_pose = "south"
-            elif cur_pose == "south":
-                commands.append(-2)
-                cur_pose = "west"
-        prev = A[i]
+#             commands.append(abs(prev[1] - A[i][1])) 
+#         # 1/4 circle moves
+#         elif prev[0] != A[i][0] and prev[1] != A[i][1]:
+#             if cur_pose == "west":
+#                 commands.append(-3)
+#                 cur_pose = "south"
+#             elif cur_pose == "east":
+#                 commands.append(-2)
+#                 cur_pose = "south"
+#             elif cur_pose == "south":
+#                 commands.append(-2)
+#                 cur_pose = "west"
+#         prev = A[i]
 
-    return commands
+#     return commands
 
 
-# A = [[-5,-15],[15,-15],[15,15], [-10,15] ,[-15,10] ,[-15,5], [0,5] ,[5, 0] , [0,-5], [-10,-5] ,[-15,-1] ,[-15,-1]]
-A = [[-5,-15],[15,-15],[15,15], [-10,15] ,[-15,10] ,[-15,5], [0,5] ,[5, 0] , [0,-5], [-10,-5] ,[-15,-10] ,[-15,-15]]
-commands = [20, -1, 30, -1, 25, -3, 5, -1, 15, -2, -2, 10, -3, 5]
-print(getRobotCommands(A))
+# # A = [[-5,-15],[15,-15],[15,15], [-10,15] ,[-15,10] ,[-15,5], [0,5] ,[5, 0] , [0,-5], [-10,-5] ,[-15,-1] ,[-15,-1]]
+# A = [[-5,-15],[15,-15],[15,15], [-10,15] ,[-15,10] ,[-15,5], [0,5] ,[5, 0] , [0,-5], [-10,-5] ,[-15,-10] ,[-15,-15]]
+# commands = [20, -1, 30, -1, 25, -3, 5, -1, 15, -2, -2, 10, -3, 5]
+# print(getRobotCommands(A))
 
+import math
+
+# # goal -15, 10
+# pose = [-10, 15]
+# angle = math.pi/2
+# R = 5
+# time = 4.822344723260332
+# x = R*math.sin(angle)
+# y = R*(1+math.cos(angle))
+
+# print(x, y)
+
+s= 5*math.pi/2
+R  =5
+angle = s/R
+x = R*math.cos(angle)
+y = R*math.sin(angle)
+# print(x, y)
